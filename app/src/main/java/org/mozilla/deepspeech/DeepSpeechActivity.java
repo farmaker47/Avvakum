0e0fc616563e15e9eb9e62301109fda102663108
farmaker47 <farmaker47@gmail.com>
2019-12-01 08:58:08 +0200

    Migrate to androidX

--- app/src/main/java/org/mozilla/deepspeech/DeepSpeechActivity.java
+++ app/src/main/java/org/mozilla/deepspeech/DeepSpeechActivity.java
@@ -1,6 +1,6 @@
 package org.mozilla.deepspeech;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;

a61258e0d73ce615058abb958269a5fc8cfcf098
farmaker47 <farmaker47@gmail.com>
2019-12-21 13:16:46 +0200

    Changes

--- app/src/main/java/org/soloupis/deepspeech/DeepSpeechActivity.java
+++ app/src/main/java/org/soloupis/deepspeech/DeepSpeechActivity.java
@@ -114,9 +114,9 @@ public class DeepSpeechActivity extends AppCompatActivity {
                                               }
                                           },
                             //Set how long before to start calling the TimerTask (in milliseconds)
-                            7000,
+                            27000,
                             //Set the amount of time between each execution (in milliseconds)
-                            7000);
+                            27000);
 
                 } else {
                     rippleBackground.stopRippleAnimation();
